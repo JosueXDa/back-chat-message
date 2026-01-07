995509c09e216e6c6832978edb6753ffef23345d
Josue Guallichico <josueloy12@gmail.com>
2026-01-06 20:48:21 -0500

    refactor(users): remove UserRepository and its exports

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,14 @@
 import type { Context, Next } from "hono";
 import { auth } from "../lib/auth";
 
-export type SessionContext = NonNullable<Awaited<ReturnType<typeof auth.api.getSession>>>;
+export type SessionContext = 
+    NonNullable<
+        Awaited<
+            ReturnType<
+                typeof auth.api.getSession
+            >
+        >
+    >;
 
 export type AuthVariables = {
     session: SessionContext;
--- src/modules/users/repositories/user.repository.ts
+++
@@ -1,2 +0,0 @@
-export { IUserRepository } from './user.repository.interface';
-export { UserRepositoryImpl } from './impl/user.repository.impl';
